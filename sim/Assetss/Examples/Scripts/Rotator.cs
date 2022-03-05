e686c38bd56730c0756fffec66b829a29334713c
ALİ ER <58634753+Xerewulf@users.noreply.github.com>
2022-03-05 17:44:43 +0300

    Add files via upload

---
+++ sim/Assetss/Examples/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()// rotate the object around x and y axes
+    {
+        transform.localRotation *= Quaternion.AngleAxis(speed * Time.deltaTime , Vector3.up);
+    }
+}
